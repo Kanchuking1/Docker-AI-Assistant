cfc6e6e8663a107b52f0e0822584ce71570dc8fa
Ace <51465755+AceVikings@users.noreply.github.com>
2023-11-07 21:25:42 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,11 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { HumanMessage, SystemMessage } from "langchain/schema";
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+// import { HumanMessage } from "langchain/dist/schema";
 dotenv.config();
 
 const app = new express();
@@ -19,9 +21,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const systemMessage = new SystemMessage(
+  "You are DockerGPT, a helpful Docker Assistant AI. Your job is to provide concise answers to user's questions. If the answer is a code snippet, strictly reply with that unless asked to explain it as welld."
+);
 app.post("/chat", async (req, res) => {
   const { message } = req.body;
-  const response = await chat.predict(message);
+  const text = new HumanMessage(message);
+  const response = await chat.predictMessages([systemMessage, text]);
   res.json(response);
 });
 
@@ -29,7 +35,7 @@ const embeddings = new OpenAIEmbeddings({
   openAIApiKey: process.env.OPENAI_API_KEY,
 });
 
-const PORT = process.env.PORT || 3008;
+const PORT = process.env.PORT || 3009;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
