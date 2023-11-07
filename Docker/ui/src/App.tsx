baa5d407ed15c596cef998575d716bbe627afd35
Wahib Kapdi <kanchuking1@gmail.com>
2023-11-07 18:12:20 +0530

    Running and Copying commands Added

--- Docker/ui/src/App.tsx
+++ Docker/ui/src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import Button from '@mui/material/Button';
 import { createDockerDesktopClient } from '@docker/extension-api-client';
-import { Stack, TextField, Typography, Container, Paper, Box, LinearProgress } from '@mui/material';
+import { Stack, TextField, Typography, Container, Paper, Box, LinearProgress, ButtonGroup } from '@mui/material';
 import { MuiMarkdown } from 'mui-markdown';
 
 const CHAT_ENDPOINT = "/chat";
 
-const SAMPLE_REQUEST = "Can you write a DockerFile for a project using Postgres React and Express?";
+const TEST_QUERY = "Can you write a DockerFile for a project using Postgres React and Express?";
 
-const TEST_RESPONSE = "\"Sure! Here's an example Dockerfile for a project using Postgres, React, and Express:\n\nDockerfile\\n# Stage 1 - Build React app\\nFROM node:14 as react-build\\nWORKDIR /app\\nCOPY package*.json /app/\\nRUN npm install\\nCOPY . /app\\nRUN npm run build\\n\\n# Stage 2 - Build Express server\\nFROM node:14 as express-build\\nWORKDIR /app\\nCOPY package*.json /app/\\nRUN npm install\\nCOPY . /app\\n\\n# Stage 3 - Combine React app and Express server\\nFROM node:14\\nWORKDIR /app\\nCOPY --from=react-build /app/build /app/client/build\\nCOPY --from=express-build /app /app\\nCOPY .env.docker /app/.env\\nRUN npm run build\\n\\n# Environment variables\\nENV PORT=3000\\nENV DATABASE_URL=your_postgres_database_url\\n\\nEXPOSE $PORT\\n\\nCMD [ \\\"npm\\\", \\\"start\\\" ]\\n\n\nExplanation:\n1. In the first stage, we use a base image of node:14 to build the React app. We copy the package.json files, install the dependencies, and then copy the entire project directory into the working directory. Finally, we run the build command of the React app.\n2. In the second stage, we repeat the same steps as the first stage but for the Express server.\n3. In the final stage, we use the same node:14 base image. We copy the built React app from the first stage into the client/build directory of the working directory. We also copy the Express server files, a .env.docker file (you should create this file with your environment variables), and run the build command to install the server dependencies.\n4. We set environment variables: PORT for the server port and DATABASE_URL for your Postgres database URL.\n5. We expose the server port (change the exposed port number if necessary).\n6. Finally, we use the CMD command to start the server.\n\nMake sure to replace your_postgres_database_url and adjust the file paths and environment variables according to your project setup.\"";
+const TEST_RESPONSE = "Sure! Here's a Dockerfile that uses the latest images of Postgres and Express:\n\n```Dockerfile\n# Use the official Postgres image\nFROM postgres:latest\n\n# Set the environment variables for Postgres\nENV POSTGRES_USER postgres\nENV POSTGRES_PASSWORD password\n\n# Create a database\nENV POSTGRES_DB mydatabase\n\n# Copy the SQL script to initialize the database\nCOPY init.sql /docker-entrypoint-initdb.d/\n\n# Use the official Node.js image\nFROM node:latest\n\n# Set the working directory\nWORKDIR /app\n\n# Copy package.json and package-lock.json\nCOPY package*.json ./\n\n# Install project dependencies\nRUN npm install\n\n# Copy the application source code\nCOPY . .\n\n# Expose the port that the Express app runs on\nEXPOSE 3000\n\n# Start the Express app\nCMD [ \"npm\", \"start\" ]\n```\n\nNote: This Dockerfile assumes that you have a `init.sql` file in the same directory that contains the SQL script to initialize the database. Adjust the PostgreSQL credentials and database name as per your requirement.\n\nTo build and run the Docker image:\n\n1. Make sure you have Docker installed.\n2. Place the `Dockerfile` and `init.sql` in the same directory as your Express project.\n3. Open a terminal and navigate to the project directory.\n4. Run the following commands:\n\n```bash\n# Build the Docker image\ndocker build -t myapp .\n\n# Run the Docker container\ndocker run -p 3000:3000 myapp\n```\n\nReplace `myapp` with the desired name for your Docker image and container.";
 
 // Note: This line relies on Docker Desktop's presence as a host application.
 // If you're running this React app in a browser, it won't work properly.
@@ -18,9 +18,85 @@ function useDockerDesktopClient() {
   return client;
 }
 
+const CodeSnippet = ({ children, ...props } : {
+  children: {
+    props : {
+      children : string
+    }
+  }
+}) => {
+  const markdown = children.props.children;
+  const codeLines = markdown.split('\n');
+
+  const code = markdown.replaceAll('\n', '<br />');
+  const component = <div dangerouslySetInnerHTML={{__html: code}}></div>;
+  const actionButtons = createRef<HTMLDivElement>();
+  const ddClient = useDockerDesktopClient();
+  
+  return <div 
+  onMouseEnter={() => {
+    if (actionButtons && actionButtons.current) actionButtons.current.style.display = 'block';
+  }}
+  onMouseLeave={() => {
+    if (actionButtons && actionButtons.current) actionButtons.current.style.display = 'none';
+  }}
+  >
+    <Paper {...props}
+      sx={{
+        p: 4,
+        fontFamily: 'monospace',
+        position: 'relative'
+      }}
+    >
+      <ButtonGroup 
+        ref={actionButtons}
+        className='action-button-group'
+        sx={{
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        display: 'none'
+      }} variant="outlined" aria-label="outlined button group">
+        <Button variant="contained" onClick={() => {
+          try {
+            navigator.clipboard.writeText(markdown);
+            ddClient.desktopUI.toast.success("Code copied to clipboard");
+          } catch (error) {
+            ddClient.desktopUI.toast.error("Something went wrong");
+          }
+        }}>COPY</Button>
+      </ButtonGroup>
+      {codeLines.map(line => {
+        const words = line.split(" ");
+        if (words.length > 0 && words[0] == "docker") {
+          return <Button 
+          variant="text"
+          sx={{
+            fontFamily: "monospace",
+            ":hover" : {
+              bgcolor: "#1C262D"
+            }
+          }}
+          onClick={async () => {
+            try {
+              const output = await ddClient.docker.cli.exec(words[1], words.slice(2, words.length));
+              ddClient.desktopUI.toast.success(output);
+            } catch (error) {
+              ddClient.desktopUI.toast.error("Something went wrong");
+            }
+          }}
+          >
+            {line}
+          </Button>
+        }
+        return <div>{line}<br /></div>;
+      })}</Paper>
+    </div>;
+}
+
 export function App() {
-  const [query, setQuery] = React.useState<string>();
-  const [response, setResponse] = React.useState<string>();
+  const [query, setQuery] = React.useState<string>(TEST_QUERY);
+  const [response, setResponse] = React.useState<string>(TEST_RESPONSE);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const ddClient = useDockerDesktopClient();
 
@@ -65,7 +141,13 @@ export function App() {
           <Box sx={{ width: '100%' }}>
             <LinearProgress />
           </Box>: <Box sx={{ p : 2 }}>
-            <MuiMarkdown>
+            <MuiMarkdown
+              options={{
+                overrides: {
+                  pre : CodeSnippet
+                }
+              }}
+            >
               {response ?? ' '}
             </MuiMarkdown>
           </Box>}
